bd627639f8d28f59911802a55ea2dadb18025af9
ocramz <zocca.marco@gmail.com>
2016-05-02 16:19:08 +0200

    updated rpc call in start()

--- ipymol/ipymol/core.py
+++ ipymol/ipymol/core.py
@@ -37,7 +37,7 @@ class MolViewer(object):
             return
         self._thread.start()
         self._server = xc.ServerProxy(
-            'http://%s:%d/RPC2' % (self.host, self.port)
+            uri="http://%s:%d/RPC2" % (self.host, self.port)
         )
 
         time.sleep(1)
